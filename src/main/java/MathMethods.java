354a1592d6d6f08f1f4b3a5bd12e4fdcf436d161
mrgtal <mrgtal@gmail.com>
2017-05-16 21:11:03 +0300

    initial commit

---
+++ src/main/java/MathMethods.java
@@ -0,0 +1,11 @@
+/**
+ *  Created by oleksandr.orlov on 13.05.2017.
+ */
+public class MathMethods {
+
+    public static int CalculateSum(int a, int b) {
+        int sum = a+b;
+        return sum;
+    }
+
+}
---
+++ src/test/java/MathMethodsTest.java
@@ -0,0 +1,13 @@
+/**
+ * Created by 3oleksandr.orlov on 13.05.2017.
+ */
+public class MathMethodsTest {
+
+    public static void main(String[] args) {
+
+//        int x = MathMethods.CalculateSum(5, 11);
+
+        System.out.println(MathMethods.CalculateSum(5, 11));
+    }
+
+}
